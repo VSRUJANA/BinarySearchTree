b78f973710862ccd436f3838040c8fb1445d3f38
VSRUJANA <srujana.valavala@gmail.com>
2020-10-25 18:24:38 +0530

    [srujana] Add. Welcome message added

--- DS_BinarySearchTree/Program.cs
+++ DS_BinarySearchTree/Program.cs
@@ -6,7 +6,7 @@ namespace DS_BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Binary Search Tree program!");
         }
     }
 }
